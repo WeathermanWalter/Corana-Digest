4a4bacfd0da7751dbf2f586d25caa25926103552
David Wordelman <Davidwordelman@gmail.com>
2020-07-03 23:20:48 -0700

    [ADDED] area ArrayList now gets filled properly

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,11 @@ package com.company;
 
 import CSVReader.FileRead;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.StoredConfig;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -11,7 +15,7 @@ import java.util.Map;
 
 public class Main {
     static ArrayList<String> dateStamps = new ArrayList<String>();
-    static ArrayList<Place> areas = new ArrayList<Place>();
+    static ArrayList<Place> areas = new ArrayList<Place>(58);
     static Map<String, String> map = new HashMap<String, String>(); //assigns each csv to its time stamp
 
     public static void main(String[] args) throws IOException {
@@ -21,17 +25,42 @@ public class Main {
     }
 
     static void updateRepo() {
+        File f = new File("RAW data");
+            try {
+                Git git = Git.cloneRepository()
+                        .setURI("https://github.com/CSSEGISandData/COVID-19.git") //this is the covid repo, updated daily
+                        .setDirectory(new File("RAW data")) //perhaps change it to f?
+                        .call();
+            } catch (GitAPIException e) {
+                e.printStackTrace();
+                System.out.println("Could not update from online, please check your internet connection");
+                System.out.println("exiting");
+                System.exit(1);
+            }
+
+    }
+
+    static boolean checkContents(File f) {
+        boolean bool = f.mkdir();
+        if (bool) return false; //if it did make a dir, nothings in it, and needs to be filled
         try {
-            Git git = Git.cloneRepository()
-                    .setURI("https://github.com/CSSEGISandData/COVID-19.git")
-                    .setDirectory(new File("C:\\Users\\david\\Desktop\\Corona_digest\\RAW data"))
-                    .call();
-        } catch (GitAPIException e) {
+            FileRepositoryBuilder builder = new FileRepositoryBuilder();
+            Repository repo = builder.setGitDir(f)
+                    .readEnvironment()
+                    .findGitDir()
+                    .build();
+
+            // setting remote server so wa can "pull" from github
+            Git git = new Git(repo);
+            PullCommand cmd = git.pull();
+            cmd.setRemote("https://github.com/CSSEGISandData/COVID-19.git");
+
+            System.out.println(cmd.getRemote());
+
+        } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Could not update from online, please check your internet connection");
-            System.out.println("exiting");
-            System.exit(1);
         }
+        return true;
     }
 
     private static void readFile() throws IOException {
@@ -54,9 +83,42 @@ public class Main {
         //loop through most recent csv file, and start assigning places
         String buffer = map.get(dateStamps.get(dateStamps.size() - 1));
         CSVReader reader = new CSVReader(buffer);
+
+        //first line will be the entry names
         for (int i = 0; i < 18; i++) {
-            System.out.println(reader.getNextValue());
+            System.out.print(reader.getNextValue() + ",");
         }
+
+        String entry;
+        while (true) {
+            entry = reader.getNextValue();
+            if(entry.equals("\0")) break; //if our reader returns null, then the reader closed its stream and nothing left is to be read
+
+            //following code is a bit messy, but I dont know any better way to go at this
+            Place p = new Place();
+            p.setProvince_State(entry); //our first word is alread here, we needed to test if it was the end of the file
+            p.setCountry_Region(reader.getNextValue());
+            p.setLast_Update(reader.getNextValue());
+            p.setLat(reader.getNextValue());
+            p.setLong_(reader.getNextValue());
+            p.setConfirmed(reader.getNextValue());
+            p.setDeaths(reader.getNextValue());
+            p.setRecovered(reader.getNextValue());
+            p.setActive(reader.getNextValue());
+            p.setFIPS(reader.getNextValue());
+            p.setIncident_Rate(reader.getNextValue());
+            p.setPeople_Tested(reader.getNextValue());
+            p.setPeople_Hospitalized(reader.getNextValue());
+            p.setMortality_Rate(reader.getNextValue());
+            p.setUID(reader.getNextValue());
+            p.setISO3(reader.getNextValue());
+            p.setTesting_Rate(reader.getNextValue());
+            p.setHospitalization_Rate(reader.getNextValue());
+            System.out.println(p.toString());
+
+            areas.add(p);
+        }
+
     }
 
     private static String trimExtension(String input) {
@@ -81,10 +143,12 @@ public class Main {
                 while (true) {
                     value = br.read();
                     char c = (char)value;
-                    if (c == ',') {
+                    if (c == ',' || c == '\n') {
                         break;
-                    } else if (c == '\n') {
-                        break;
+                    }
+                    if(c == '\0') {
+                        br.close();
+                        return "\n";
                     }
                     sb.append(c);
                 }
--- src/com/company/Place.java
+++ src/com/company/Place.java
@@ -35,64 +35,172 @@ public class Place {
         Last_Update = i;
     }
 
-    static void setLat(double i) {
-        Lat = i;
+    static void setLat(String i) {
+        Lat = checkDouble(i);
     }
 
-    static void setLong_(double i) {
-        Long_ = i;
+    static void setLong_(String i) {
+        Long_ = checkDouble(i);
     }
 
-    static void setConfirmed(int i) {
-        Confirmed = i;
+    static void setConfirmed(String i) {
+        Confirmed = checkInt(i);
     }
 
-    static void setDeaths(int i) {
-        Deaths = i;
+    static void setDeaths(String i) {
+        Deaths = checkInt(i);
     }
 
-    static void setRecovered(int i) {
-        Recovered = i;
+    static void setRecovered(String i) {
+        Recovered = checkInt(i);
     }
 
-    static void setActive(double i) {
-        Active = i;
+    static void setActive(String i) {
+        Active = checkDouble(i);
     }
 
-    static void setFIPS(int i) {
-        FIPS = i;
+    static void setFIPS(String i) {
+        FIPS = checkInt(i);
     }
 
-    static void setIncident_Rate(double i) {
-        Incident_Rate = i;
+    static void setIncident_Rate(String i) {
+        Incident_Rate = checkDouble(i);
     }
 
-    static void setPeople_Tested(int i) {
-        People_Tested = i;
+    static void setPeople_Tested(String i) {
+        People_Tested = checkInt(i);
     }
 
-    static void setPeople_Hospitalized(int i) {
-        People_Hospitalized = i;
+    static void setPeople_Hospitalized(String i) {
+        People_Hospitalized = checkInt(i);
     }
 
-    static void setMortality_Rate(double i) {
-        Mortality_Rate = i;
+    static void setMortality_Rate(String i) {
+        Mortality_Rate = checkDouble(i);
     }
 
-    static void setUID(int i) {
-        UID = i;
+    static void setUID(String i) {
+        UID = checkInt(i);
     }
 
     static void setISO3(String i) {
         ISO3 = i;
     }
 
-    static void setTesting_Rate(double i) {
-        Testing_Rate = i;
+    static void setTesting_Rate(String i) {
+        Testing_Rate = checkDouble(i);
     }
 
-    static void setHospitalization_Rate(double i) {
-        Hospitalization_Rate = i;
+    static void setHospitalization_Rate(String i) {
+        Hospitalization_Rate = checkDouble(i);
+    }
+
+    private static double checkDouble(String num) {
+        if (num.equals("")) return 0;
+        else return Double.parseDouble(num);
+    }
+
+    private static int checkInt(String num) {
+        if (num.equals("")) return 0;
+        else return Integer.parseInt(num);
+    }
+
+    //--------------------------------------------------------------------------
+
+    static String getProvince_State() {
+        return Province_State;
+    }
+
+    static String getCountry_Region() {
+        return Country_Region;
+    }
+
+    static String getLast_Update() {
+        return Last_Update;
+    }
+
+    static double getLat() {
+        return Lat;
+    }
+
+    static double getLong_() {
+        return Long_;
+    }
+
+    static int getConfirmed() {
+        return Confirmed;
+    }
+
+    static int getDeaths() {
+        return Deaths;
+    }
+
+    static int getRecovered() {
+        return Recovered;
+    }
+
+    static double getActive() {
+        return Active;
+    }
+
+    static int getFIPS() {
+        return FIPS;
+    }
+
+    static double getIncident_Rate() {
+        return Incident_Rate;
+    }
+
+    static int getPeople_Tested() {
+        return People_Tested;
+    }
+
+    static int getPeople_Hospitalized() {
+        return People_Hospitalized;
+    }
+
+    static double getMortality_Rate() {
+        return Mortality_Rate;
+    }
+
+    static int getUID() {
+        return UID;
+    }
+
+    static String getISO3() {
+        return ISO3;
+    }
+
+    static double getTesting_Rate() {
+        return Testing_Rate;
+    }
+
+    static double getHospitalization_Rate() {
+        return Hospitalization_Rate;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("*********************************");
+        sb.append("\nState = " + getProvince_State());
+        sb.append("\nCountry = " + getCountry_Region());
+        sb.append("\nLast Update = " + getLast_Update());
+        sb.append("\nLatitude = " + Double.toString(getLat()));
+        sb.append("\nLongitude = " + Double.toString(getLong_()));
+        sb.append("\nConfirmed cases = " + Integer.toString(getConfirmed()));
+        sb.append("\nDeaths = " +Integer.toString(getDeaths()));
+        sb.append("\nRecovered = " +Integer.toString(getRecovered()));
+        sb.append("\nActive cases = " + Double.toString(getActive()));
+        sb.append("\nFIPS = " + Integer.toString(getFIPS()));
+        sb.append("\nIncident Rate = " + Double.toString(getIncident_Rate()));
+        sb.append("\nPeople Tested = " + Integer.toString(getPeople_Tested()));
+        sb.append("\nPeople Hospitalized = " + Integer.toString(getPeople_Hospitalized()));
+        sb.append("\nMortality Rate = " + Double.toString(getMortality_Rate()));
+        sb.append("\nUID = " + Integer.toString(getUID()));
+        sb.append("\nISO3 = " + getISO3());
+        sb.append("\nTesting Rate = " + Double.toString(getTesting_Rate()));
+        sb.append("\nHospitalization rate = " + Double.toString(getHospitalization_Rate()));
+        return sb.toString();
     }
 
 }
